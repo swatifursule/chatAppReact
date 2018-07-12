7c09cd1e00355eaafe16a01a6829849574aeddce
Swati Fursule <swati@silvertrak.com.au>
2018-07-12 14:44:39 +1000

    chat application

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/1568a46a-93c9-4657-973e-d21bd3d48312/token";
+const instanceLocator = "v1:us1:1568a46a-93c9-4657-973e-d21bd3d48312";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
